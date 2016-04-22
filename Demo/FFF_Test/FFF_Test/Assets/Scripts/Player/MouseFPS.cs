ac46a09be252dffc0e038f7b64fb169f29534c5a
Legobyggarn <a14augka@hs.local>
2016-04-22 14:10:17 +0200

    Arm shoot animation
    Animation fixes

---
+++ Demo/FFF_Test/FFF_Test/Assets/Scripts/Player/MouseFPS.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFPS : MonoBehaviour {
+
+
+    // Character controller
+    private CharacterController mCharacterController;
+
+    // Rotation
+    private float rotX;
+    private float rotY;
+    public float rotSpeedX;
+    public float rotSpeedY;
+    public float rotSpeed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        rotX = transform.localRotation.eulerAngles.x;
+        rotY = transform.localRotation.eulerAngles.y;
+
+        // Character controller
+        mCharacterController = GetComponent<CharacterController>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Rotate camera (Player)
+        rotX += -Input.GetAxis("Mouse Y") * rotSpeedX;
+        rotY += Input.GetAxis("Mouse X") * rotSpeedY;
+
+        transform.localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+
+      
+
+    }
+
+    
+
+}
