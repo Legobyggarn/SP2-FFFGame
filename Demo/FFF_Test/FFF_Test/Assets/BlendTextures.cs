540e9a712d44a6812793ed2c8bcf22c78b3222d8
Legobyggarn <a14augka@hs.local>
2016-05-02 16:01:29 +0200

    Added new idle animation and lots of trial and error
    Idle animation 10 is implemented
    Tested different ways on how to change textures on arms!

---
+++ Demo/FFF_Test/FFF_Test/Assets/BlendTextures.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlendTextures : MonoBehaviour {
+    public Material material1;
+    public Material material2;
+    public float lerp;
+    public Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.material = material1;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rend.material.Lerp(material1, material2, lerp);
+    }
+}
