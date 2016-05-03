6749a908c92614a6612a1210cd05d9b1e835b00b
TheZilanze <mattias.svensson.bergqvist@gmail.com>
2016-05-03 16:05:06 +0200

    Loading screen, load scene in the background
    Loading screen, load scene in the background.
    
    - Add | Code to the 'loadingScreen' script making it possible to load
    the next scene asynchronously, in the background. While still being able
    to show the player something else, loading progress for example.

--- Demo/FFF_Test/FFF_Test/Assets/Scripts/Loading/loadingScreen.cs
+++ Demo/FFF_Test/FFF_Test/Assets/Scripts/Loading/loadingScreen.cs
@@ -1,23 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class loadingScreen : MonoBehaviour {
 
 	public ScenesTransision st;
 
 	private float sceneChangeTime;
-	private float maxSceneChangeTime = 6f;
+	private float maxSceneChangeTime = 10f;
 
 	private float sceneFadeTime;
 	private float maxSceneFadeTime;
 
 	private bool fading = true;
 
+	// Scene loading
+	private bool loaded = false;
+	public string levelName;
+	private AsyncOperation asyncLoad;
+
 	// Use this for initialization
 	void Start () 
 	{
 		st.fadeFromWhite ();
 		maxSceneFadeTime = st.getLerpTime();
+
+		// Start loading
+		StartCoroutine("load");
 	}
 	
 	// Update is called once per frame
@@ -25,7 +34,8 @@ public class loadingScreen : MonoBehaviour {
 	{
 		sceneChangeTime += Time.deltaTime;
 
-		if (maxSceneChangeTime < sceneChangeTime) 
+		// If level loaded fade to white and start level
+		if (maxSceneChangeTime < sceneChangeTime && loaded) // (maxSceneChangeTime < sceneChangeTime)
 		{ 
 			if (fading) 
 			{
@@ -33,10 +43,35 @@ public class loadingScreen : MonoBehaviour {
 				fading = false;
 			}
 			sceneFadeTime += Time.deltaTime;
+
 			if (maxSceneFadeTime < sceneFadeTime) 
 			{
-				Application.LoadLevel ("Scene_01");
+				// Start the loaded level...
+				asyncLoad.allowSceneActivation = true;
+
+				//Application.LoadLevel ("Scene_01");
+			}
+		}
+
+		// Print loading process
+		//Debug.Log("Loading: " + async.progress + "%");
+
+	}
+
+	// Coroutine for loading
+	IEnumerator load() {
+		// Debug log warning?
+		//Debug.Log("Loading scene " + levelName);
+		asyncLoad = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
+		asyncLoad.allowSceneActivation = false;
+		while (!loaded) {
+			Debug.Log ("Loading: " + asyncLoad.progress);
+			yield return null;
+			// Work around for bug stopping loading at 0.9
+			if (asyncLoad.progress >= 0.9f) {
+				loaded = true;
 			}
 		}
 	}
+
 }
